372e14bf389e1a262c45ad2de07525ee3381f3df
Simran Akshay Karode <simranbhav004@gmail.com>
2024-03-08 14:17:54 +0530

    Last Commit

---
+++ Reminder/ReminderViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ReminderViewCell.swift
+//  Reminder
+//
+//  Created by Simran on 06/03/24.
+//
+
+import UIKit
+
+class ReminderViewCell: UITableViewCell {
+    
+    @IBOutlet weak var ReminderDate: UILabel!
+    
+    @IBOutlet weak var ReminderTitle: UILabel!
+}
---
+++ Reminder/TaskModule.swift
@@ -0,0 +1,14 @@
+//
+//  TaskModule.swift
+//  Reminder
+//
+//  Created by Simran on 06/03/24.
+//
+
+import Foundation
+
+struct ReminderModel {
+    let title: String
+    let date: Date
+    let identifier: String
+}
